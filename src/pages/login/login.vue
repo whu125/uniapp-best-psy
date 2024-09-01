fefffdd6e230d6d251521783d5458d677929675d
whu125 <2691806221@qq.com>
2024-09-01 22:17:33 +0800

    改写登录

--- src/pages/login/login.vue
+++ src/pages/login/login.vue
@@ -35,7 +35,10 @@
             </view>
 
             <view style="margin-top: 20px">
-              <wd-button block @click="wiexinLogin" type="success">微信一键登陆</wd-button>
+              <!-- <wd-button block @click="weixinLogin" type="success">微信一键登陆</wd-button> -->
+              <wd-button block open-type="getPhoneNumber" @getphonenumber="getPhoneNumber">
+                微信一键登录
+              </wd-button>
             </view>
           </view>
         </wd-tab>
@@ -50,7 +53,7 @@
 <script lang="ts" setup>
 import PLATFORM from '@/utils/platform'
 import { useUserStore } from '@/store'
-import { getUserInfo } from '@/service/index/user'
+import { getUserInfo, getPhoneNumberApi } from '@/service/index/user'
 
 defineOptions({
   name: 'login',
@@ -74,14 +77,48 @@ const login = () => {
   console.log('登录123')
 }
 
-const wiexinLogin = () => {
+// const openid = ref()
+
+const getPhoneNumber = async (e) => {
+  // 在bindgetphonenumber回调中获取code动态令牌
+  const res = await weixinLogin()
+
+  console.log('获取手机号的动态令牌:', e) // 动态令牌
+  const openid = userStore.userInfo.userId
+  const code = e.code
+
+  getPhoneNumberApi(openid, code).then((res2) => {
+    // 服务端获取手机号
+    if (res2.code == 200) {
+      uni.showToast({
+        title: '登录成功',
+      })
+    }
+  })
+  //   // console.log(res, '接口换取的openid')
+  //   console.log('获取手机号的动态令牌:', e.detail.code) // 动态令牌
+  //   getPhoneNumberFn(e.detail.code, res.openid).then((res2) => {
+  //     // 服务端获取手机号
+  //     if (res2.code == 0) {
+  //       uni.setStorageSync('phoneNumber', res.content.phone_info.phoneNumber)
+  //       uni.showToast({
+  //         title: '登录成功',
+  //       })
+  //     }
+  //   })
+  // })
+}
+
+const weixinLogin = async () => {
   console.log('weixin login')
   uni.login({
     provider: 'weixin',
     onlyAuthorize: true, // 微信登录仅请求授权认证
     success: function (event) {
+      console.log(event)
       const { code } = event
       console.log(code)
+
       // 客户端成功获取授权临时票据（code）,向业务服务器发起登录请求。
       uni.request({
         url: 'http://localhost:8081/loginByWechat',
@@ -90,6 +127,7 @@ const wiexinLogin = () => {
           code: event.code,
         },
         success: async (res) => {
+          console.log('res', res)
           // 获得token完成登录
           const token = res.data.data
           userStore.setUserToken(token)
